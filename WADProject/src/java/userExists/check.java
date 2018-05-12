80c43e56fa3799a19a3a66e0fa37dc3cbbfe93f1
maria.fatu <fatu_maria2001@yahoo.com>
2018-05-13 01:02:56 +0300

    change to the DB schema name

--- WADProject/src/java/userExists/check.java
+++ WADProject/src/java/userExists/check.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package userExists;
 
 import java.sql.Connection;
@@ -10,16 +6,13 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 /**
  *
  * @author SUPER
  */
 public class check {
-    String url = "jdbc:mysql://localhost:3306/test";
+    String url = "jdbc:mysql://localhost:3306/wadproject";
     public boolean userExists(String username) throws SQLException, ClassNotFoundException{
 
         Class.forName("com.mysql.jdbc.Driver"); 
@@ -27,7 +20,6 @@ public class check {
         java.sql.Statement instr = con.createStatement();
         String sql = "SELECT username FROM user";
         Statement statement2 = con.createStatement();
-        //String sql2="INSERT INTO test.`user` (username, password) VALUES ('aura', 'aura')";
         ResultSet rs = instr.executeQuery(sql);
         while(rs.next()){
             String name=rs.getString(1);
@@ -47,7 +39,6 @@ public class check {
         ResultSet rs = instr.executeQuery(sql);
         while(rs.next()){
             String username=rs.getString(1);
-            //String password=rs.getString(2);
             if(user.equals(username)) return true;
         }
         return false;
